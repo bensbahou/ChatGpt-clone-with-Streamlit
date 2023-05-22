778bd1469eed5715481a653cfb45e58f8d0d1b00
bensbahou <younesbensbahou@gmail.com>
2023-05-22 12:56:25 +0100

    active chat with memory

--- gpt_clone.py
+++ gpt_clone.py
@@ -2,13 +2,22 @@ import streamlit as st
 from streamlit_chat import message
 from dotenv import load_dotenv
 import os
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage,
+)
 
-load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+def init():
+    load_dotenv()
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    st.set_page_config(page_title="ChatGPT Clone", page_icon=":robot_face:")
 
-st.set_page_config(page_title="ChatGPT Clone", page_icon=":robot_face:")
-# st.write(OPENAI_API_KEY[0:3] + "*" * 32 + OPENAI_API_KEY[-3:])
+
+init()
+
 st.title("ChatGPT Clone :robot_face:")
 
 with st.sidebar:
@@ -16,5 +25,22 @@ with st.sidebar:
     st.write("This is a clone of [ChatGPT](https://chat.openai.com/)")
     user_input = st.text_input("You", value="", key="user_input", type="default")
 
-message("Hello, How can I help you today?")
-message("I'm user", is_user=True)
+
+chat = ChatOpenAI(temperature=0.9)
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        SystemMessage(
+            content="You're now connected to a random person for a text chat."
+        ),
+    ]
+if user_input:
+    message(user_input, is_user=True)
+    st.session_state.messages.append(HumanMessage(content=user_input))
+    with st.spinner("Thinking..."):
+        response = chat(messages=st.session_state.messages)
+    message(response.content)
+    st.session_state.messages.append(AIMessage(content=response.content))
+
+
+# write messages contents in the end of the page
+st.write(st.session_state.messages)
