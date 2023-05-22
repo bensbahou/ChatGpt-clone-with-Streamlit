44a0b6473d788715fa19c84121e3056917dc188a
bensbahou <younesbensbahou@gmail.com>
2023-05-22 12:15:11 +0100

    added static chat

---
+++ gpt_clone.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from streamlit_chat import message
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+st.set_page_config(page_title="ChatGPT Clone", page_icon=":robot_face:")
+st.write(OPENAI_API_KEY)
+st.title("ChatGPT Clone :robot_face:")
+
+with st.sidebar:
+    st.write("## About")
+    st.write("This is a clone of [ChatGPT](https://chat.openai.com/)")
+    user_input = st.text_input("You", value="", key="user_input", type="default")
+
+message("Hello, How can I help you today?")
+message("I'm user", is_user=True)
